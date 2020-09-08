ea8df9160099267288486ed86cbc7c97bffe23ce
Christine Martino <christine.martino@thinkcompany.com>
2020-09-08 17:22:57 -0400

    Add Homework folder and assignment (#5)
    * Add Jest and package.json to core project
    
    * Add homework files and tests
    
    * Add homework Readme and an additional function test

---
+++ homework/data-wrangling/data-wrangling.js
@@ -0,0 +1,16 @@
+// Import the list of posts from posts.json.
+
+// Return only the posts written by specific User ID
+export const getPosts = () => {
+  throw new Error("Remove this statement and implement this function");
+};
+
+// Return only the titles of posts by specific User ID
+export const getTitles = () => {
+  throw new Error("Remove this statement and implement this function");
+};
+
+// Return a list of all authors' names
+export const getNames = () => {
+  throw new Error("Remove this statement and implement this function");
+};
