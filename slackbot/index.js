fc44f42bc1a2ac3543750f78717ac407469b79c1
Christine Martino <christine.martino@thinkcompany.com>
2020-09-07 21:18:47 -0400

    Add doggo Pic message

--- slackbot/index.js
+++ slackbot/index.js
@@ -1,8 +1,27 @@
 require("dotenv").config();
 const { App } = require("@slack/bolt");
-// const axios = require("axios");
+const axios = require("axios");
+
+const composeMessage = (imageURL, text) => {
+  return {
+    blocks: [
+      {
+        type: "image",
+        image_url: imageURL,
+        alt_text: text || "Cute Corgi!",
+      },
+    ],
+  };
+};
 
 // Dog API Endpoint: https://dog.ceo/api/breed/corgi/images/random
+const fetchDoggo = () =>
+  axios("https://dog.ceo/api/breed/corgi/images/random")
+    .then(function (response) {
+      // handle success
+      return composeMessage(response.data.message);
+    })
+    .catch((err) => err);
 
 // Authorize Slackbot
 const bot = new App({
@@ -11,12 +30,14 @@ const bot = new App({
 });
 
 // Respond to Events
-bot.event("app_mention", ({ context, event }) => {
+bot.event("app_mention", async ({ context, event, say }) => {
   try {
+    const doggo = await fetchDoggo();
     bot.client.chat.postMessage({
       token: context.botToken,
       channel: event.channel,
-      text: `Hey yoo <@${event.user}> you mentioned me`,
+      text: "Hi",
+      ...doggo,
     });
   } catch (e) {
     console.log(`error responding ${e}`);
