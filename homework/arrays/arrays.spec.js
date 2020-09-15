1536e37542aa6a26152c5b241c5439695c98f282
Christine Martino <christine.martino@thinkcompany.com>
2020-09-14 20:58:55 -0400

    Add array homework

---
+++ homework/arrays/arrays.spec.js
@@ -0,0 +1,19 @@
+import {
+  inventors,
+  people,
+  data,
+  inventorsBirthYear,
+  inventorNames,
+  inventorBirthYears,
+  inventorTotalYears,
+  inventorYearsLived,
+  parisianBoulevards,
+  peopleSorted,
+  dataInstances,
+} from "./arrays";
+
+describe("Inventors", () => {
+  test("List inventors born in the 1500's", () => {
+    expect(inventorsBirthYear()).toEqual();
+  });
+});
