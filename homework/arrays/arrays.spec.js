58e0b5bfb2ea9e24d5f01c21279801f2321935ed
Christine Martino <christine.martino@thinkcompany.com>
2020-09-14 23:50:57 -0400

    Add result for brith Year

--- homework/arrays/arrays.spec.js
+++ homework/arrays/arrays.spec.js
@@ -14,6 +14,9 @@ import {
 
 describe("Inventors", () => {
   test("List inventors born in the 1500's", () => {
-    expect(inventorsBirthYear()).toEqual();
+    expect(inventorsBirthYear()).toEqual([
+      { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+      { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+    ]);
   });
 });
